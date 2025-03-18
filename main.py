18cdf2442d84a12dc355e132c532965d9f15e4f9
Sear <73845169+SSeeaarr@users.noreply.github.com>
2025-03-18 00:56:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import sys
 
 def adminperms():
     if ctypes.windll.shell32.IsUserAnAdmin():
-        return  # Already admin, continue with the rest of the script.
+        return
     
     params = " ".join(f'"{arg}"' for arg in sys.argv)
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, params, None, 1)
-    sys.exit()  # Exit to restart with elevated privileges.
+    sys.exit()
 
 if not ctypes.windll.shell32.IsUserAnAdmin():
     adminperms()
